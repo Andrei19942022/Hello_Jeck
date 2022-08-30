575a95dc99a10bf71cdb1e4412c6164a7e08f72b
Anderson <eremeevandrej012@gmail.com>
2022-08-30 11:01:10 +0300

    Первый файл

---
+++ урок9.1функции.py
@@ -0,0 +1,24 @@
+# def a_function():
+#     print('You just created a function!')
+#
+# a_function()
+
+
+# Пустая функция
+# def empty_function():
+#     pass
+
+# задача1 Создайте функцию,которая будет считать сумму чисел в массиве
+# def a():
+#     sum = 0
+#     for i in range(10):
+#         sum += 1
+#         print(sum)
+#
+# a()
+
+# передача аргументов функции
+# def add(a,b):
+#     return a + b
+#
+# print(add(1,2))
